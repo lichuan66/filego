16a39c378ff3555586ff00fe0bfea1bcaaba3f2c
lca233 <9115856+lca233@user.noreply.gitee.com>
2024-08-14 23:57:43 +0800

    fix: 更新packages

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -1,8 +1,9 @@
 import express, { Request, Response } from "express";
-import initMongoDB from "@filego/database/mongoose/initMongoDB";
+// import initMongoDB from "@filego/database/mongoose/initMongoDB";
 import userRoutes from "./routes/user/user";
 import { Router } from "express";
 import cors from "cors";
+import path from "path";
 
 const app = express();
 const port = 2333;
@@ -12,10 +13,11 @@ type routeListType = [string, Router][];
 const routesPath: routeListType = [["/api/user", userRoutes]];
 
 (async () => {
-  await initMongoDB();
+  // await initMongoDB();
 
   app.use(cors());
   app.use(express.json());
+  app.use(express.static(path.join(__dirname, "public")));
 
   routesPath.forEach((routes) => {
     app.use(routes[0], routes[1]);
