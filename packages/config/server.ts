288469426d2f2f7166bce78409c6d7c2aefaed6c
lca233 <9115856+lca233@user.noreply.gitee.com>
2024-08-15 22:47:06 +0800

    fix: 更新服务端配置，生产环境连接到远程数据库

--- packages/config/server.ts
+++ packages/config/server.ts
@@ -1,5 +1,12 @@
-// const { env } = process;
+import ip from "ip";
+
+const { env } = process;
 
 export default {
-  database: "mongodb://127.0.0.1:27017/filego",
+  /** 服务端host，默认为本机ip地址（可能会是局域网地址） */
+  host: env.Host || ip.address(),
+
+  port: env.Port ? parseInt(env.Port, 10) : 2333,
+
+  database: env.Database || "mongodb://127.0.0.1:27017/filego",
 };
