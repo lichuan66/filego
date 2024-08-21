3a7a13e6a10a0036e3e5b6478513ddb2cd345626
lca233 <9115856+lca233@user.noreply.gitee.com>
2024-08-21 19:36:57 +0800

    fix: 更新生产环境和开发环境ip

--- packages/config/client.ts
+++ packages/config/client.ts
@@ -1,3 +1,7 @@
+const server = `${window.location.host.split(":")[0]}:${window.location.port}`;
+
+console.log("NODE_ENV ===>", process.env.NODE_ENV);
+
 export default {
-  Server: "192.168.1.4:2333",
+  Server: process.env.NODE_ENV === "ddevelopment" ? "192.168.1.4:2333" : server,
 };
