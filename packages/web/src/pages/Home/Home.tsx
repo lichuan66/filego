76f47a64032d811d536b72bd7345e78bf4842ab2
lca233 <9115856+lca233@user.noreply.gitee.com>
2024-09-22 20:59:20 +0800

    fix: build web

--- packages/web/src/pages/Home/Home.tsx
+++ packages/web/src/pages/Home/Home.tsx
@@ -14,7 +14,7 @@ export interface modeType {
 }
 
 export default function Home() {
-  console.log(111222);
+  console.log(1112223333);
 
   const pageType = usePageType();
   const [modeTypeIndex, setModeTypeIndex] = useState("1");
