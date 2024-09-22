6e8ccc0c6cb7cbcf9c6f49d7b46b0b432b453fe3
lca233 <9115856+lca233@user.noreply.gitee.com>
2024-09-22 19:55:55 +0800

    fix: 测试web构建镜像6

--- packages/web/src/pages/Home/Home.tsx
+++ packages/web/src/pages/Home/Home.tsx
@@ -14,7 +14,7 @@ export interface modeType {
 }
 
 export default function Home() {
-  console.log(111222);
+  console.log(111222333);
 
   const pageType = usePageType();
   const [modeTypeIndex, setModeTypeIndex] = useState("1");
